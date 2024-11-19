07c399345c906dd17de779d08525a5914da8467e
Harsh Srivastava <harshme08@gmail.com>
2024-11-20 00:16:19 +0530

    style changed

--- index.js
+++ index.js
@@ -26,37 +26,37 @@ document.addEventListener("keypress", function(event) {
 function makeSound(key) {
 
   switch (key) {
-    case "w":
+    case "do":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
-    case "a":
+    case "re":
       var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
 
-    case "s":
+    case "mi":
       var tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
       break;
 
-    case "d":
+    case "fa":
       var tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
       break;
 
-    case "j":
+    case "so":
       var snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
 
-    case "k":
+    case "la":
       var crash = new Audio('sounds/crash.mp3');
       crash.play();
       break;
 
-    case "l":
+    case "ti":
       var kick = new Audio('sounds/kick-bass.mp3');
       kick.play();
       break;
